389a048605684795df8a397ac34f2a41721c246e
trapt0r <brent@cdo.linux.org.ph>
2025-05-18 09:39:03 -0400

    Update AppTest.java

--- src/java/main/example/AppTest.java
+++ src/java/main/example/AppTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class AppTest {
+
     @Test
-    public void testMocking() {
-        Runnable r = mock(Runnable.class);
-        r.run();
-        verify(r).run();
+    void stressMockito() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            Runnable r = mock(Runnable.class);
+            r.run();
+            verify(r).run();
+        }
+        Thread.sleep(500); // simulate processing delay
     }
 }
